21b9fc5d1e68c142ccea50eab35587cc7c13984c
orestznak <51721977+orestznak@users.noreply.github.com>
2022-06-16 10:17:00 +0300

    завдання1
    скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const ref = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
 
+};
+
+let intervalId = null;
+
+ref.btnStart.addEventListener('click', onStart);
+ref.btnStop.addEventListener('click', onStop);
+
+function onStart(evt) {
+    evt.preventDefault();
+    ref.btnStart.style.disabled = false;
+    ref.btnStart.style.color =  '#666666';
+
+    intervalId = setInterval(() => {
+        const changeColor = getRandomHexColor();
+        ref.body.style.background = changeColor;
+    },1000);
+};
+
+function onStop(evt) {
+    evt.preventDefault();
+    clearInterval(intervalId);
+
+    ref.btnStart.style.disabled = true;
+    ref.btnStart.style.color =  '#000000';
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
