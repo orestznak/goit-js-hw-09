e676ce43369528944d019db74e281177c955b5bb
orestznak <51721977+orestznak@users.noreply.github.com>
2022-06-16 16:36:21 +0300

    завдання3
    скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
+  btnSubmit: document.querySelector('button'),
+  form: document.querySelector('form')
+}
+
+
+
+refs.form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  let delayUser = parseInt(refs.inputDelay.value);
+  const amountUser = parseInt(refs.inputAmount.value);
+  const stepUser = parseInt(refs.inputStep.value);
+
+  for(let position = 1; position <= amountUser; position += 1) {
+    createPromise (position, delayUser)
+      .then(value => {
+        Notiflix.Notify.success(value);;
+      })
+      .catch(error => {
+        Notiflix.Notify.warning(error);
+      });
+    delayUser +=stepUser;
+  }
+
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+    return new Promise((resolve, reject) =>{
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);          
+        } else {
+          reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        }
+      }, delay);
+    });
 }
